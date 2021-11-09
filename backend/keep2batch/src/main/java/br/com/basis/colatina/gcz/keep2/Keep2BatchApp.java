bc4e4d29a3a3f1a8079e177e617b665e56f380a8
gudeck <guzuc@outlook.com>
2021-11-09 19:00:46 -0300

    configurando keep2batch

--- backend/keep2batch/src/main/java/br/com/basis/colatina/gcz/keep2/Keep2BatchApp.java
+++ backend/keep2batch/src/main/java/br/com/basis/colatina/gcz/keep2/Keep2BatchApp.java
@@ -6,20 +6,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@EnableConfigurationProperties({LiquibaseProperties.class})
+@EnableConfigurationProperties({})
 @RequiredArgsConstructor
 @Slf4j
-public class Keep2App implements InitializingBean {
+public class Keep2BatchApp implements InitializingBean {
 
     private final Environment env;
 
     public static void main(String[] args) {
-        AppUtil.startup(args, Keep2App.class, log);
+        AppUtil.startup(args, Keep2BatchApp.class, log);
     }
 
     @Override
